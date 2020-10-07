cd63fab9464bab285697c27ae5f84ab7596e2fc0
Slay3r <Yoo@myhub.com>
2020-10-06 21:22:30 -0400

    simple hashing using c++ stl

---
+++ beginner/string_hashing.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std; 
+
+
+int main(){
+	string s;
+	cin >>s;
+	map<char, int> mpp; 
+	for (auto it: s){
+		mpp[it]++;
+	}
+	for (char c='a'; c<='z'; c++){
+		cout <<c <<" ->" << mpp[c]<<"\n";
+	}
+
+	return 0;
+}
+
